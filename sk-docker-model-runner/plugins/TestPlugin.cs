8abe654adbbae8289652fc5076d1bfe628354961
ajaykatta <katta.ajay@live.com>
2025-09-09 21:16:57 -0500

    added test plugin

---
+++ sk-docker-model-runner/plugins/TestPlugin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.SemanticKernel;
+
+namespace sk_docker_model_runner.plugins;
+
+public class TestPlugin
+{
+    [KernelFunction("echo")]
+    public string Echo(string input)
+    {
+        return input;
+    }
+}
