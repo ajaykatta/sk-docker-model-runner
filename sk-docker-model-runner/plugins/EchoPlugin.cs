6c746db70f44b2dac729a06bbdeb9d8338e95348
ajaykatta <katta.ajay@live.com>
2025-09-11 08:32:24 -0500

    Added Echo plugin

---
+++ sk-docker-model-runner/plugins/EchoPlugin.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace sk_docker_model_runner.plugins;
+
+public class EchoPlugin
+{
+    public EchoPlugin()
+    {
+    }
+
+    [KernelFunction("echo")]
+    [Description("Echoes the input string")]
+    public string Echo(string input)
+    {
+        return input;
+    }    
+}
